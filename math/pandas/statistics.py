9e2480e4178269de1f3c8ab8cb432bdccc8d2e98
Basil Minkov <proveyourselfmail@gmail.com>
2019-05-22 11:52:52 +0300

    added some documentation and rmse function

--- math/pandas/statistics.py
+++ math/pandas/statistics.py
@@ -11,7 +11,7 @@ def standardize(df):
 
     Parameters
     ----------
-    df : pandas Data Frame
+    df : pandas.DataFrame, numpy.array
 	
     '''
     return (df - df.mean()) / df.std()
@@ -29,10 +29,42 @@ def normalize(df):
 
     Parameters
     ----------
-    df : pandas Data Frame
+    df : pandas.DataFrame, numpy.array
 
 
     '''
     return (df - df.mean()) / df.std()
 
+def rmse(df_1, df_2):
+	'''
+	The root-mean-square error (RMSE) is a frequently used measure of 
+	the differences between values (sample or population values) predicted by 
+	a model or an estimator and the values observed. The RMSD represents 
+	the square root of the second sample moment of the differences between 
+	predicted values and observed values or the quadratic mean of these differences. 
+	These deviations are called residuals when the calculations are performed over 
+	the data sample that was used for estimation and are called errors (or prediction errors) 
+	when computed out-of-sample. The RMSD serves to aggregate the magnitudes of the errors in 
+	predictions for various times into a single measure of predictive power. 
+	RMSD is a measure of accuracy, to compare forecasting errors of different models 
+	for a particular dataset and not between datasets, as it is scale-dependent.
+
+	Equation
+	________
+	$RMSE = $
+
+	Reference
+	_________
+	1.	Hyndman, Rob J.; Koehler, Anne B. (2006). "Another look at measures of forecast accuracy". 
+		International Journal of Forecasting. 22 (4): 679–688. CiteSeerX 10.1.1.154.9771. 
+		doi:10.1016/j.ijforecast.2006.03.001.
+
+    Parameters
+    ----------
+    df1 : pandas.DataFrame, numpy.array
+    df2 : pandas.DataFrame, numpy.array
+
+	'''
+
+	return ((df_1 - df_2)**2).mean()**0.5
 
--- math/regression.py
+++ math/regression.py
@@ -5,7 +5,7 @@ from .curves import sine_wave
 
 def sin_regression(x, y):
     '''
-    Fit sin to the input time sequence, and return fixing parameters 
+    Fit sine to the input time sequence, and return fixing parameters 
     "amp", "omega", "phase", "offset", "freq", "period" and "fitfunc"
 
     Parameters
@@ -18,17 +18,18 @@ def sin_regression(x, y):
 
     '''
     
+    # make numpy array if list
     x = np.array(x)
     y = np.array(y)
-    ff = np.fft.fftfreq(len(x), (x[1]-x[0])) # assume uniform spacing
+    ff = np.fft.fftfreq(len(x), (x[1]-x[0])) # assume uniform spacing of argument
     Fy = abs(np.fft.fft(y))
-    guess_freq = abs(ff[numpy.argmax(Fy[1:])+1]) # excluding the zero frequency "peak", which is related to offset
+    guess_freq = abs(ff[np.argmax(Fy[1:])+1]) # excluding the zero frequency "peak", which is related to offset
     guess_amp = np.std(y) * 2.**0.5
     guess_offset = np.mean(y)
-    guess = np.array([guess_amp, 2.*numpy.pi*guess_freq, 0., guess_offset])
+    guess = np.array([guess_amp, 2.*np.pi*guess_freq, 0., guess_offset])
 
-    popt, pcov = scipy.optimize.curve_fit(sin_wave, x, y, p0=guess)
+    popt, pcov = scipy.optimize.curve_fit(sine_wave, x, y, p0=guess)
     A, w, p, c = popt
-    f = w/(2.*numpy.pi)
+    f = w/(2.*np.pi)
     fitfunc = lambda t: A * np.sin(w*t + p) + c
-    return {"amp": A, "omega": w, "phase": p, "offset": c, "freq": f, "period": 1./f, "fitfunc": fitfunc, "maxcov": numpy.max(pcov), "rawres": (guess,popt,pcov)}
+    return {"amp": A, "omega": w, "phase": p, "offset": c, "freq": f, "period": 1./f, "fitfunc": fitfunc, "maxcov": np.max(pcov), "rawres": (guess,popt,pcov)}
